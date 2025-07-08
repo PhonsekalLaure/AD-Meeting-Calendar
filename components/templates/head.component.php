509c217b933dbe2786b7e52b1b764900ed9dbf68
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-08 12:06:43 +0800

    feat: add head and foot template components for HTML structure

---
+++ components/templates/head.component.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once UTILS_PATH . "/htmlEscape.util.php";
+
+function head($pageTitle, array $pageCss = [])
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+        <head>
+            <meta charset="utf-8">
+            <title><?= htmlEscape($pageTitle ?? 'My Callendar') ?></title>
+
+            <?php
+            if (!empty($pageCss)) {
+                foreach ($pageCss as $cssFile) {
+                    echo "<link rel=\"stylesheet\" href=\"{$cssFile}\">\n";
+                }
+            }
+            ?>
+
+        </head>
+
+        <body>
+            <main>
+                <?php
+}
+?>
