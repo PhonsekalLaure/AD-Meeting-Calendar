dfc37e8b8a4c79be26c7565cde87ee4ad32b2a78
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-08 12:06:12 +0800

    feat: add constants for templates and layouts paths in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,7 @@ define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
+define('TEMPLATES_PATH', BASE_PATH . '/components/templates/');
+define('LAYOUTS_PATH' , BASE_PATH . '/layouts/');
 
 chdir(BASE_PATH);
