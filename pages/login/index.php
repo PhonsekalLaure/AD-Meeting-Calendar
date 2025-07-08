9bc77bd6a8bb3d8df7afd8b3433f07a01b945da6
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-08 12:07:22 +0800

    feat: implement main layout and styling for login and success pages

--- pages/login/index.php
+++ pages/login/index.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>SUCCESS!!</h1>
-    <form action="/handlers/auth.handler.php?action=logout" method="POST">
-        <button type="submit">Logout</button>
-    </form>
-</body>
-</html>
+<?php
+require_once LAYOUTS_PATH . "/main.layout.php";
+
+$title = "Meeting Callendar";
+$css = ['css' => ['/assets/css/style.css']];
+
+renderMainLayout(
+    function (){
+        ?>
+        <div class="success-container">
+            <h1>Login Success</h1>
+        </div>
+    <?php
+    },
+    $title,
+    $css
+);
+?>
